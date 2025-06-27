678d86d609b461c88d4f5b00b51fda421441d576
Anuraj Gogoi <anurajgogoi01@gmail.com>
2025-06-27 19:04:17 +0530

    env updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ import { app, server } from "./lib/socket.js";
 import path from "path";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = 5001;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); // or more if needed
